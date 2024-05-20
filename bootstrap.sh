9f96b86e14fd0a2d64a52fd1e56961bed10f41f4
nhanluongoe <nhanlt18@gmail.com>
2024-05-20 16:52:19 +0700

    remove confirmation

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,17 +10,12 @@ function doIt() {
 		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
+		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
+	source ~/.bash_profile;
 	source ~/.zshrc;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
+doIt;
+
 unset doIt;
